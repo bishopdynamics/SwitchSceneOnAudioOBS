890e97c5e5cb5bd0b85121b769e2120d091fd983
James Bishop <james@bishopdynamics.com>
2021-03-26 15:40:40 -0700

    tweaks, better error message on websocket errors

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@ const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
 const OBSWebSocket = require('obs-websocket-js');
+const { send } = require('process');
+
+/*
+    Below are configuration variables, to be customized to suit your needs
+*/
+
+const active_state = 'expanded'; // name of the filter to activate when they are talking
+const inactive_state = 'normal'; // name of filter when not talking
+const quiet_time = 4000; // how long input is quiet before switching to inactive state
+const interval_period = 150; // how often to run the changeScene function
+const websocket_host = 'localhost' // address of machine running obs with websocket plugin
+const websocket_port = 4444; // port where obs websocket plugin is running
+const websocket_password = 'secret'; // password for websocket
+const webserver_port = 3000; // port to serve the http gui
+
+/*
+    End configuration variables, do not edit below here
+*/
+
 
 const obs = new OBSWebSocket();
-obs.connect({ address: 'localhost:4444', password: 'secret' });
+obs.connect({ address: websocket_host + ':' + websocket_port, password: websocket_password });
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(express.static('./public'));
-
 
 //Convert this in iterables and dynamic elemnts
 slave1 = {}
@@ -20,12 +38,6 @@ slave3 = {}
 slave4 = {}
 slave5 = {}
 master = {}
-
-// These are the configuration variables
-const active_state = 'expanded'; // name of the filter to activate when they are talking
-const inactive_state = 'normal'; // name of filter when not talking
-const quiet_time = 4000; // how long input is quiet before switching to inactive state
-const interval_period = 150; // how often to run the changeScene function
 
 // this tracks how many milliseconds since the indicated source went quiet, aka dropped below the threshold for activation/expansion
 state_tracker_quiet = {};
@@ -36,8 +48,7 @@ state_tracker_state = {};
 io.on('connection', (socket) => {
     socket.on('audioInput', (body) => {
         setValues({ 'volume': parseInt(body.volume), 'id': body.id, 'scene': body.scene, 'limit': parseInt(body.limit)})
-        console.log({ 'volume': parseInt(body.volume), 'id': body.id, 'scene': body.scene, 'limit': parseInt(body.limit)})
-
+        //console.log({ 'volume': parseInt(body.volume), 'id': body.id, 'scene': body.scene, 'limit': parseInt(body.limit)})
     });
 });
 
@@ -62,6 +73,14 @@ function setValues(val) {
     }
 }
 
+function sendOBSSourceFilterMessage(source_name, target_state) {
+    try {
+        obs.send('SetSourceFilterVisibility', {'sourceName': source_name, 'filterName': target_state, 'filterEnabled': true});
+    } catch {
+        console.warn('failure sending obs source filter message');
+    }
+}
+
 function setActive(source_name) {
     // send active state signal
     if (!(source_name in state_tracker_state)) {
@@ -69,7 +88,7 @@ function setActive(source_name) {
         state_tracker_state[source_name] = 'inactive';
     } else {
         if (state_tracker_state[source_name] == 'inactive'){
-            obs.send('SetSourceFilterVisibility', {'sourceName': source_name, 'filterName': active_state, 'filterEnabled': true});
+            sendOBSSourceFilterMessage(source_name, active_state);
             state_tracker_quiet[source_name] = 0;
             state_tracker_state[source_name] = 'active';
         }
@@ -94,7 +113,7 @@ function setInactive(source_name) {
         state_tracker_state[source_name] = 'inactive';
     } else {
         if (state_tracker_state[source_name] == 'active'){
-            obs.send('SetSourceFilterVisibility', {'sourceName': source_name, 'filterName': inactive_state, 'filterEnabled': true});
+            sendOBSSourceFilterMessage(source_name, inactive_state);
             state_tracker_quiet[source_name] = 0;
             state_tracker_state[source_name] = 'inactive';
         }
@@ -140,9 +159,19 @@ function changeScene() {
 }
 
 setInterval(() => {
-    changeScene()
+    try {
+        changeScene();
+    } catch {
+        console.warn('changeScene threw an exception')
+    }
 }, interval_period);
 
-server.listen(3000/*3000*/, () => {
-    console.log('OBS Audio Switch')
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason.stack);
+    // application specific logging, throwing an error, or other logic here
+  });
+
+server.listen(webserver_port, () => {
+    console.log('OBS Audio Switch is now running on port ' + webserver_port)
+    console.log('')
 });
